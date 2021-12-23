0584063041917c0afb5ad8d8bb7dbb36e7813a74
SHREYASH SRIVASTAVA <39947281+shreyashsrivastava@users.noreply.github.com>
2021-12-24 02:52:29 +0530

    Maintaining the compatibility with the Django
    the `smart_text` has been removed from the latest versions of Django, we can user `smart_str`.
    and `ugettext_lazy` is now `gettext`.

--- rest_framework_jwt/authentication.py
+++ rest_framework_jwt/authentication.py
@@ -1,8 +1,8 @@
 import jwt
 
 from django.contrib.auth import get_user_model
-from django.utils.encoding import smart_text
-from django.utils.translation import ugettext as _
+from django.utils.encoding import smart_str as smart_text
+from django.utils.translation import gettext as _
 from rest_framework import exceptions
 from rest_framework.authentication import (
     BaseAuthentication, get_authorization_header
